001f9eab96768b98452fe2004bd96a44093082e0
Huntandy <10.hitmen.reborn@gmail.com>
2023-12-28 17:29:36 +0800

    responsive js year  in the copyright line was added

--- Processing Project/js/script.js
+++ Processing Project/js/script.js
@@ -1,18 +1,3 @@
-// // Get a reference to the <a> tag and the target <div>
-// const scrollLink = document.getElementById('about');
-// const targetDiv = document.getElementById('bout-me');
-
-// // Add a click event listener to the <a> tag
-// scrollLink.addEventListener('click', function (event) {
-//     event.preventDefault(); // Prevent the default behavior of the link (e.g., navigating to a new page)
-
-//     // Scroll to the target <div>
-//     targetDiv.scrollIntoView({
-//         behavior: 'smooth', // You can change this to 'auto' for instant scrolling
-//         block: 'start',     // Scroll to the top of the target div
-//     });
-// });
-
 // Get references to all the elements with the class "scroll-target"
 const scrollTargets = document.querySelectorAll('.scroll-target');
 
@@ -32,6 +17,18 @@ document.querySelectorAll('a[data-target]').forEach(function (link) {
         });
     });
 });
+
+// //----------------------------------------------------------
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+const copyrightElement = document.getElementById("copyright");
+const updatedContent = copyrightElement.textContent.replace("©", `© ${year}`);
+copyrightElement.textContent = updatedContent;
+
+
+
 // //----------------------------------------------------------
 let scrollTop = document.querySelector('.scroll-top');
 
